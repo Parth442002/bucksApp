8d64c940f11de501708412c82ed539656f1be15a
Bhurva Sharma <me@bhurvasharma.com>
2023-03-31 16:55:10 +0530

    login screen

--- src/Screens/Auth/login.screen.js
+++ src/Screens/Auth/login.screen.js
@@ -1,27 +1,60 @@
-import { View, Text,TextInput,StyleSheet, TouchableOpacity } from 'react-native'
+import { Image,  View, Text,TextInput,StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const LoginScreen = () => {
   return (
-    <KeyboardAwareScrollView
-      //style={{ backgroundColor: '#4c69a5' }}
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={{flex:1,alignItems:"center",justifyContent:"center"}}
-      scrollEnabled={false}
-    >
-      <Text>Login Form</Text>
+    
+      
+        
+        
+  <KeyboardAwareScrollView
+   
+   resetScrollToCoords={{ x: 0, y: 0 }}
+   contentContainerStyle={{flex:1,alignItems:"center",justifyContent:"center"}}
+   scrollEnabled={false}
+ >
+
+  
+
+<Text style= {{fontSize: 32, fontWeight:500, paddingBottom: 20}}>Welcome Back! </Text>
+
+
+
+  
+<Image style={styles.image} source = {require("../../Assets/Images/Logo.png")}/>
+      
+      <View style={styles.inputView}>
         <TextInput
-          placeholder="Email/Phone"
-          //style={styles.input}
+          style= {styles.inputView}
+          
+          placeholder='Email/Phone No.'
+          placeholderTextColor= '003f5c'
+          onChangeText={(email) => setEmail(email)}
         />
+        </View>
+
+        <View style={styles.inputView}>
         <TextInput
-          placeholder="Password"
-          //style={styles.input}
+          style= {styles.inputView}
+          placeholder='Password'
+          placeholderTextColor= '003f5c'
+          secureTextEntry={true}
+
+
+          onChangeText={(password) => setPassword(password)}
         />
-        <TouchableOpacity>
-          <Text>Login</Text>
-        </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text> 
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}> Forgot Password? </Text> 
+      </TouchableOpacity>
+
+
     </KeyboardAwareScrollView>
   )
 }
@@ -31,5 +64,68 @@ export default LoginScreen
 
 
 const styles = StyleSheet.create({
+
+
+
+  image: {
+    width: 190,
+    height: 180,
+    marginBottom: 50,
+    
+
+
+  },
+  inputView: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent:"center",
+  },
+
+  inputView: {
+    backgroundColor: "#fff",
+    
+    borderRadius: 7
+    ,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    backgroundColor: "#FF6A4A",
+
+  },
+  loginText:{
+    fontWeight:500,
+    color: "#fff",
+    
+  },
+
+  forgot_button: {
+    marginTop:20,
+    height: 40,
+    marginBottom: 60,
+    
+  },
+
+
+
+
 
 })
